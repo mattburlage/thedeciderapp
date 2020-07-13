b8810af6278e01daaf83c9034cfd5a264199df49
matt burlage <mattburlage@gmail.com>
2020-07-13 05:57:58 -0400

    change campaign url

--- decider/urls.py
+++ decider/urls.py
@@ -4,7 +4,7 @@ from decider import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<slug:campaign_code>', views.campaign, name='campaign'),
+    path('campaign/<slug:campaign_code>', views.campaign, name='campaign'),
     path('vote/<int:item_id>/<slug:vote_val>', views.vote, name='vote'),
     path('add/<int:camp_id>', views.add_item, name='add'),
     path('clear/<int:item_id>', views.clear_vote, name='clear_vote'),
