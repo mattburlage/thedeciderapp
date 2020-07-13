1aa74a015a79986ee965732f6df00e6a4e6373ea
matt burlage <mattburlage@gmail.com>
2020-07-13 06:03:56 -0400

    changed admin url

--- thedeciderapp/urls.py
+++ thedeciderapp/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('hotjambalaya/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('decider.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
