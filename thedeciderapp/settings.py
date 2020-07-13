3b97e94444f56728b70dd00a11633f9d563a9122
matt burlage <mattburlage@gmail.com>
2020-07-13 06:35:41 -0400

    added logout functionality

--- thedeciderapp/settings.py
+++ thedeciderapp/settings.py
@@ -128,5 +128,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
