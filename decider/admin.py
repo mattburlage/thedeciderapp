9520bc51616cfd345a45d488923df1e1ad6f73a0
matt burlage <mattburlage@gmail.com>
2020-07-13 06:02:13 -0400

    reverted admin.py

--- decider/admin.py
+++ decider/admin.py
@@ -4,8 +4,17 @@ from django.contrib.admin.sites import AlreadyRegistered
 
 from decider.models import Campaign
 
+models = apps.get_models()
+
 
 @admin.register(Campaign)
 class CampaignAdmin(admin.ModelAdmin):
     filter_horizontal = ['subscribers']
 
+
+app_models = apps.get_app_config('decider').get_models()
+for model in app_models:
+    try:
+        admin.site.register(model)
+    except AlreadyRegistered:
+        pass
