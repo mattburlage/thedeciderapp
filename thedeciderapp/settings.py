5df9454cfe1791a4ae8601295ac8e354af9cb4a9
matt burlage <mattburlage@gmail.com>
2020-07-13 05:59:52 -0400

    added static root

--- thedeciderapp/settings.py
+++ thedeciderapp/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = "media/"
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 LOGIN_REDIRECT_URL = '/'
